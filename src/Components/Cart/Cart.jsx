2ae2bb7711291f78ace958c66961ac3f6b1e7676
Ziad-Mohammed-Ezzat1712 <201927109@std.sci.cu.edu.eg>
2025-11-13 15:33:23 +0200

    edite

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -134,7 +134,7 @@ export default function Cart() {
   <input
     type="text"
     placeholder="Coupon code"
-    className="w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+    className="w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#790000]"
   />
 </div>
 
--- src/Components/shippingPage/shippingPage.jsx
+++ src/Components/shippingPage/shippingPage.jsx
@@ -107,7 +107,7 @@ export default function ShippingPage() {
   <input
     type="text"
     placeholder="Coupon code"
-    className="w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+    className="w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#790000]"
   />
 </div>
 
