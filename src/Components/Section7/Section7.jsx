2e95a5cc32ecba4c906edabdfe8efd84976864ad
Ziad-Mohammed-Ezzat1712 <ziadmohammed1712@gmail.com>
2026-03-24 20:30:50 +0200

    api integration9

--- src/Components/Section7/Section7.jsx
+++ src/Components/Section7/Section7.jsx
@@ -190,7 +190,7 @@
 //   useEffect(() => {
 //     axios
 //       .get(
-//         `/api/products/getallproducts.php?nocache=${Date.now()}`
+//         `https://dashboard.splash-e-liquid.com/products/getallproducts.php?nocache=${Date.now()}`
 //       )
 //       .then((res) => {
 //         if (res.data.status) {
@@ -352,7 +352,7 @@ export default function Section7() {
   // ================= FETCH PRODUCTS =================
   useEffect(() => {
     axios
-      .get(`/api/products/getallproducts.php?nocache=${Date.now()}`)
+      .get(`https://dashboard.splash-e-liquid.com/products/getallproducts.php?nocache=${Date.now()}`)
       .then((res) => {
         if (res.data.status) {
 
